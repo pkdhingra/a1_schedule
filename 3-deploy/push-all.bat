4a9f02d9a0fd10a078942552e7294f06117886c2
prashant dhingra <prashantkdhingra@gmail.com>
2024-10-12 23:31:30 +0000

    added by job

---
+++ 3-deploy/push-all.bat
@@ -0,0 +1,33 @@
+echo "pull all started" >> c:\e\a1_schedule\log.txt
+call kreatewebsite-push roadhana.com
+call kreatewebsite-push abexperiment.com
+call kreatewebsite-push blog.apartments-sale.com
+call kreatewebsite-push business.interior-designer.apartments-sale.com
+REM call kreatewebsite-push www.dataknobs.com
+call kreatewebsite-push dataknobs.com.dataproduct
+REM REM call kreatewebsite-push daynightcricket.com
+call kreatewebsite-push happydiwali.org
+call kreatewebsite-push holi.festivals.kreatewebsites.com
+REM REM call kreatewebsites-push indiacitytrip
+REM REM call kreatewebsites-push jewellery
+call kreatewebsite-push kreate.cloud
+REM REM call kreatewebsites-push kreatebots.com
+call kreatewebsites-push kreatebots.com.howto
+call kreatewebsites-push kreatedatasets.com
+call kreatewebsites-push slides.kreatewebsites.com
+call kreatewebsites-push makarsankranti.net
+call kreatewebsites-push makarsankranti.org
+call kreatewebsites-push sankranti.net
+REM below need output content
+REM kreatewebsites-push lowback.org   add in github
+call kreatewebsite-push 1000placestosee.info
+call kreatewebsite-push bestech.org
+call kreatewebsite-push2 dataproduct.net
+REM call kreatewebsite-push happy-new-year.org
+call kreatewebsites-push lohri.org
+call kreatewebsites-push tax-income.com
+call kreatewebsites-push tax-income.net
+call kreatewebsites-push tax-income.org
+call kreatewebsites-push slides.kreatewebsites.com
+echo "pull all done" >> c:\e\a1_schedule\log.txt
+echo "pull all complete" > c:\e\a1_schedule\log-latest.txt
---
+++ copy-articles/happydeepawali_org.happydeepawali-local.bat
@@ -0,0 +1,9 @@
+SET site=happydeepawali.org
+SET drive=happydeepawali_gdrive
+SET article=happydeepawali_prompt
+SET cms=festivals
+SET topic=festivals
+call .\copy_article_to_site-local.bat %site% %drive% %article% %cms% %topic%
+
+
+
